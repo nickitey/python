8a712ede9c9051687f104e4409123dea597cb668
Nikita Savin <klaster26@mail.ru>
2023-12-12 14:53:47 +0300

    fix: add brackets into task 4
    Human readable code and guidelines!

--- task4.py
+++ task4.py
@@ -19,7 +19,7 @@
 
 year = int(input())
 if 1900 <= year <= 3000:
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         print('Високосный')
     else:
         print('Обычный')
