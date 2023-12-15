76fa59787f0bd5539d69cc200b056b9cfa80b4c6
Nikita Savin <klaster26@mail.ru>
2023-12-15 19:01:31 +0300

    feat: solve task 25 (introduction to functions)

---
+++ task25.py
@@ -0,0 +1,34 @@
+# Напишите функцию f(x), которая возвращает значение следующей функции, определённой на всей числовой прямой:
+# f(x) = {(1-(x+2)**2, при x <= -2), (-x\2, при -2 < x <= 2), ((x-2)**2 + 1, при х>2)}
+# Требуется реализовать только функцию, решение не должно осуществлять операций ввода - вывода.
+#
+# Sample Input 1:
+# 4.5
+# Sample Output 1:
+# 7.25
+
+# Sample Input 2:
+# -4.5
+# Sample Output 2:
+# -5.25
+
+# Sample Input 3:
+# 1
+# Sample Output 3:
+# -0.5
+
+def f(x):
+    # put your python code here
+    if x <= -2:
+        return 1 - (x + 2) ** 2
+    elif -2 < x <= 2:
+        return -x / 2
+    else:
+        return (x - 2) ** 2 + 1
+
+
+assert f(4.5) == 7.25
+assert f(-4.5) == -5.25
+assert f(1) == -0.5
+
+print('All tests passed')
