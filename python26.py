26df8a5b9649e39c7e7ddb5aa3439895794127d1
Nikita Savin <klaster26@mail.ru>
2023-12-15 19:30:40 +0300

    feat: solve task 26 (modifying list)
    Wow, Python allows you to compare lists directly! It's awesome!
    JS-douchbags feel jelaous now.

---
+++ python26.py
@@ -0,0 +1,38 @@
+# Напишите функцию modify_list(l), которая принимает на вход список целых чисел,
+# удаляет из него все нечётные значения, а чётные нацело делит на два.
+# Функция не должна ничего возвращать, требуется только изменение переданного списка, например:
+
+# lst = [1, 2, 3, 4, 5, 6]
+# print(modify_list(lst))  # None
+# print(lst)               # [1, 2, 3]
+# modify_list(lst)
+# print(lst)               # [1]
+
+# lst = [10, 5, 8, 3]
+# modify_list(lst)
+# print(lst)               # [5, 4]
+# Функция не должна осуществлять ввод/вывод информации.
+
+def modify_list(l):
+    # put your python code here
+    i = 0
+    while i < len(l):
+        if l[i] % 2 != 0:
+            del l[i]
+            i -= 1
+        else:
+            l[i] = l[i] // 2
+        i += 1
+
+
+lst = [1, 2, 3, 4, 5, 6]
+modify_list(lst)
+assert lst == [1, 2, 3]
+lst = [1, 2, 3]
+modify_list(lst)
+assert lst == [1]
+lst = [10, 5, 8, 3]
+modify_list(lst)
+assert lst == [5, 4]
+
+print('All tests passed')
