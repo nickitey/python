6ad54e57cacb64daf6d40fa22fd0daa936a9f4a1
Nikita Savin <klaster26@mail.ru>
2023-12-26 00:47:14 +0300

    fix: fix tests for second case in task 43
    Also had to add another one 'zero' inside the 'also_closest_mod_5'
    function to increase the range of programm working cases
    TBH, it is so freaky

--- task43.py
+++ task43.py
@@ -21,17 +21,17 @@ print('all tests passed')
 
 
 def also_closest_mod_5(x): # Это уже тяга к извращениям просто проснулась
-    for i in range(x, 2 ** 30): # С любовью вспомнил JS с его числом Infinity
+    for i in range(x, 2 ** 300): # С любовью вспомнил JS с его числом Infinity
         if i % 5 != 0:
             i += 1
         else:
             return i
 
 
-assert closest_mod_5(7) == 10
-assert closest_mod_5(11) == 15
-assert closest_mod_5(97) == 100
-assert closest_mod_5(105) == 105
-assert closest_mod_5(19398177912838267736571639287636) == 19398177912838267736571639287640
+assert also_closest_mod_5(7) == 10
+assert also_closest_mod_5(11) == 15
+assert also_closest_mod_5(97) == 100
+assert also_closest_mod_5(105) == 105
+assert also_closest_mod_5(19398177912838267736571639287636) == 19398177912838267736571639287640
 
 print('These tests also passed')
