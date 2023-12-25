99662054435e3b6747dc16533e8f81bfe29729b0
Nikita Savin <klaster26@mail.ru>
2023-12-26 00:41:33 +0300

    feat: add new way to solve 'pascal-triangle' puzzle

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -4,6 +4,12 @@
 # Нумерацию чисел в строке также начинают с нуля и с левого края.
 # Основная формула для расчета каждого числа в треугольнике имеет вид:
 # C(n,k) = C(n - 1, k) - C(n - 1, k - 1)
+
+
+# Земельный кодекс Российской Федерации.
+# Статья 5. Часть 3:
+# Для целей настоящего Кодекса используются следующие понятия и определения:
+# собственники земельных участков - лица, являющиеся собственниками земельных участков
 def get_num_factorial(num):
     if num == 0:
         return 1
@@ -14,14 +20,19 @@ def get_num_factorial(num):
         return num * get_num_factorial(num - 1)
 
 
-def get_num_of_combinations(num, k):
-    return int(get_num_factorial(num) / (get_num_factorial(k) * get_num_factorial(num - k)))
+def get_amount_of_combinations(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
+    else:
+        return get_amount_of_combinations(n - 1, k) + get_amount_of_combinations(n - 1, k - 1)
 
 
 def get_n_pascal_triangle_string(n):
     result = []
     for i in range(n + 1):
-        result.append(get_num_of_combinations(n, i))
+        result.append(get_amount_of_combinations(n, i))
     return result
 
 
@@ -31,9 +42,26 @@ assert get_n_pascal_triangle_string(4) == [1, 4, 6, 4, 1]
 print('all tests passed')
 
 
-# Благодаря функции range() функция, возвращающая факториал числа, без рекурсии пишется даже короче и легче
+# А ТЕПЕРЬ БЕЗ РЕКУРСИИ!
 def get_factorial_num(num):
     result = 1
     for i in range(1, num + 1):
         result *= i
     return result
+
+
+def get_num_of_combinations(num, k):
+    return int(get_factorial_num(num) / (get_factorial_num(k) * get_factorial_num(num - k)))
+
+
+def get_nth_pascal_triangle_string(n):
+    result = []
+    for i in range(n + 1):
+        result.append(get_num_of_combinations(n, i))
+    return result
+
+
+assert get_nth_pascal_triangle_string(0) == [1]
+assert get_nth_pascal_triangle_string(4) == [1, 4, 6, 4, 1]
+
+print('These tests also passed')
